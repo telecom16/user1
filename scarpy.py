9585b09a32c10d63ee0703c3d44aaa0c9720eed3
jssj16 <397261247@qq.com>
2018-04-18 23:10:14 +0800

    带翻页的爬取，爬取100条

--- scarpy.py
+++ scarpy.py
@@ -21,13 +21,14 @@ def getNewsUrlList(url):
         yield(job_name,job_co,job_area)
         
 if __name__ == '__main__':
-    baseurl = 'https://search.51job.com/list/200200,000000,0000,00,9,99,java,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-    listss = getNewsUrlList(baseurl)
+    baseurl = 'https://search.51job.com/list/200200,000000,0000,00,9,99,java,2,{page}.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
     f=open('d:/data.txt','w')
-    wf = lambda x: f.write(x+u'\r\n')
-    for a,b,c in listss:
-        wf(u'~'*100)
-        wf(a[0])
-        wf(b[0])
-        wf(c[0])
+    for i in range(1,100):
+        listss = getNewsUrlList(baseurl.format(page=i))
+        wf = lambda x: f.write(x+u'\r\n')
+        for a,b,c in listss:
+            wf(u'~'*100)
+            wf(a[0])
+            wf(b[0])
+            wf(c[0])
     f.close
--- untitled0.py
+++ untitled0.py
@@ -6,7 +6,7 @@ Created on Wed Apr 18 22:10:15 2018
 """
 
 if __name__ == '__main__':
-    baseurl = 'https://search.51job.com/list/200200,000000,0000,00,9,99,java,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='
-    for i in range(10):
-        print(i)
-    print(baseurl)
+    baseurl = "https://search.51job.com/list/200200,000000,0000,00,9,99,java,2,{a}.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
+    for i in range(1,11):
+        url = baseurl.format(a=i)
+        print(url)
